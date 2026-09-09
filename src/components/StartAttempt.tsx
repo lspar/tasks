cab53096d3303c9f51556c8e58d3ed3f89cd4d92
Lindsey <lspar@udel.edu>
2026-09-09 18:49:36 -0400

    Completed task 8

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,36 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setProgress] = useState<boolean>(false);
+    return (
+        <div>
+            Number of Attempts: {attempts}
+            <Button
+                onClick={() => {
+                    setProgress(true);
+                    setAttempts(attempts - 1);
+                }}
+                disabled={inProgress || attempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    setProgress(false);
+                }}
+                disabled={!inProgress}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    setAttempts(attempts + 1);
+                }}
+                disabled={inProgress}
+            >
+                Mulligan
+            </Button>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,32 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [diceOne, setDiceOne] = useState<number>(1);
+    const [diceTwo, setDiceTwo] = useState<number>(2);
+
+    return (
+        <div>
+            <span data-testid="left-die">{diceOne}</span>
+            <span data-testid="right-die">{diceTwo}</span>
+            <Button
+                onClick={() => {
+                    setDiceOne(d6);
+                }}
+            >
+                Roll Left
+            </Button>
+            <Button
+                onClick={() => {
+                    setDiceTwo(d6);
+                }}
+            >
+                Roll Right
+            </Button>
+            {diceOne === 1 && diceTwo === 1 ?
+                <div>Lose</div>
+            : diceOne === diceTwo ?
+                <div>Win</div>
+            :   null}
+        </div>
+    );
 }
